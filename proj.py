8974029ffcb68c7a8f3841e618427feb0a52df16
Kushal Chauhan <Kushal Chauhan>
2017-03-07 02:52:35 +0530

    almost done

--- proj.py
+++ proj.py
@@ -1,15 +1,15 @@
-from pygame import mixer
-mixer.init()
-mixer.music.load("a.mp3")
-mixer.music.play()
-input()
-for i in range(10):
-    mixer.music.pause()
-    print(mixer.music.get_pos())
-    input()
-    mixer.music.unpause()
-    input()
+##from pygame import mixer
+##mixer.init()
+##mixer.music.load("a.mp3")
+##mixer.music.play()
+##input()
+##for i in range(10):
+##    mixer.music.pause()
+##    print(mixer.music.get_pos())
+##    input()
+##    mixer.music.unpause()
+##    input()
 
 from pydub import AudioSegment
 sound = AudioSegment.from_mp3("a.mp3")
-sound.export("b.wav",format="wav") 
+##sound.export("b.wav",format="wav") 
